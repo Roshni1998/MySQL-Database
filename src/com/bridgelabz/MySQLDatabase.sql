a678ea7fd603cf56735e60ddb1d4221a736d9baf
Roshni1998 <roshnimali847@gmail.com>
2021-07-10 11:54:39 +0530

    Create an employee payroll service database

---
+++ src/com/bridgelabz/MySQLDatabase.sql
@@ -0,0 +1,24 @@
+/* Welcome to MySQL Database */
+
+/* UC-1 */
+
+mysql> CREATE database employee_payroll_service;
+Query OK, 1 row affected (0.27 sec)
+
+mysql> show databases;
++--------------------------+
+| Database                 |
++--------------------------+
+| classicmodels            |
+| employee_payroll_service |
+| information_schema       |
+| mysql                    |
+| payroll_service          |
+| performance_schema       |
+| student_personal_details |
+| sys                      |
++--------------------------+
+8 rows in set (0.00 sec)
+
+mysql> USE employee_payroll_service;
+Database changed
