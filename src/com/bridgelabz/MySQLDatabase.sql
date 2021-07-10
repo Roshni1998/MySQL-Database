af0f897930cbbaca7a3d8cf212202b60040676ae
Roshni1998 <roshnimali847@gmail.com>
2021-07-10 12:37:33 +0530

    Create employee payroll table in the payroll service database as part of CRUD Operation

--- src/com/bridgelabz/MySQLDatabase.sql
+++ src/com/bridgelabz/MySQLDatabase.sql
@@ -1,7 +1,6 @@
 /* Welcome to MySQL Database */
 
 /* UC-1 */
-
 mysql> CREATE database employee_payroll_service;
 Query OK, 1 row affected (0.27 sec)
 
@@ -24,7 +23,6 @@ mysql> USE employee_payroll_service;
 Database changed
 
 /* UC-2 */
-
 mysql> Create table employee_payroll (
     -> id int NOT NULL auto_increment,
     -> name varchar(100) NOT NULL,
@@ -44,3 +42,27 @@ mysql> desc employee_payroll;
 | start  | date         | YES  |     | NULL    |                |
 +--------+--------------+------+-----+---------+----------------+
 4 rows in set (0.09 sec)
+
+/* UC-3 */
+mysql> INSERT INTO employee_payroll
+    -> (name, salary, start) VALUES
+    -> ('Bill',1000000, '2021-07-09');
+Query OK, 1 row affected (0.28 sec)
+
+mysql> INSERT INTO employee_payroll
+    -> (name, salary, start) VALUES
+    -> ('Mark',1200000, '2021-07-09'), ('David',2200000, '2021-07-02'), ('John',5000000, '2021-04-06');
+Query OK, 3 rows affected (0.22 sec)
+Records: 3  Duplicates: 0  Warnings: 0
+
+mysql> SELECT * FROM employee_payroll;
++----+---------+---------+------------+
+| id | name    | salary  | start      |
++----+---------+---------+------------+
+|  1 | Merrisa | 3200000 | 2021-07-03 |
+|  2 | Bill    | 1000000 | 2021-07-09 |
+|  3 | Mark    | 1200000 | 2021-07-09 |
+|  4 | David   | 2200000 | 2021-07-02 |
+|  5 | John    | 5000000 | 2021-04-06 |
++----+---------+---------+------------+
+5 rows in set (0.13 sec)
